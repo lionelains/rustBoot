f9919d15fbec8d1ec830a2c7d9f502e666cdeb1c
Lionel Ains <lionel.ains@eove.fr>
2024-11-23 16:01:08 +0100

    Fixing boot freeze when update partition content is broken

--- boards/bootloaders/stm32h723/src/main.rs
+++ boards/bootloaders/stm32h723/src/main.rs
@@ -9,15 +9,19 @@ use rustBoot_update::update::{update_flash::FlashUpdater, UpdateInterface};
 
 use cortex_m_rt::entry;
 
+use panic_probe as _;
+
 #[entry]
 fn main() -> ! {
     let updater = FlashUpdater::new(FlashWriterEraser::new());
     updater.rustboot_start()
 }
 
+/*
 #[panic_handler] // panicking behavior
 fn panic(_: &core::panic::PanicInfo) -> ! {
     loop {
         cortex_m::asm::bkpt();
     }
 }
+*/
